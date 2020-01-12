32f9cdcfdafcbcc05f99eec1c86e8866b1f71e41
pavitkalra1 <pavitchitkara@gmail.com>
2020-01-12 17:09:58 +0530

    Updates

---
+++ precache-manifest.0d654e04e24633903fdfea6c72d6509b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9da9fa32b5b10308bc2e715f112667d",
+    "url": "/NBA-REPO/index.html"
+  },
+  {
+    "revision": "8b5dd95a035b796146f8",
+    "url": "/NBA-REPO/static/css/main.3ffab2af.chunk.css"
+  },
+  {
+    "revision": "6d0863e8535a7b5bdf3d",
+    "url": "/NBA-REPO/static/js/2.6fe2f5a0.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/NBA-REPO/static/js/2.6fe2f5a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b5dd95a035b796146f8",
+    "url": "/NBA-REPO/static/js/main.915ae16b.chunk.js"
+  },
+  {
+    "revision": "2435323748af64ac98c4",
+    "url": "/NBA-REPO/static/js/runtime-main.bdf58d3e.js"
+  }
+]);
